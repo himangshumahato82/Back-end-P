518e478d2675b78fe717fbf7e43e49c5ae800a7d
himangshumahato82 <himangshumahato82@gmail.com>
2023-04-17 22:04:37 +0530

    update

--- ExpressMiddleware/apps.js
+++ ExpressMiddleware/apps.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 
-// Middleware function to log requests
+
 app.use((req, res, next) => {
   console.log(`Request received: ${req.method} ${req.url}`);
   next();
 });
 
-// Route handler
+
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
